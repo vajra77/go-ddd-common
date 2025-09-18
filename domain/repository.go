bc8e783a608281fc7f86bf310913d824528ad27d
vajra77 <f.ferreri@namex.it>
2025-09-18 08:08:31 +0200

    sketched repository funcs

--- domain/repository.go
+++ domain/repository.go
@@ -1,1 +1,13 @@
 package domain
+
+import "github.com/google/uuid"
+
+type Repository struct{}
+
+type IRepository interface {
+	List() []Entity
+	Retrieve(uuid.UUID) Entity
+	Add(Entity)
+	Remove(uuid.UUID)
+	Filter(func(Entity) bool) []Entity
+}
